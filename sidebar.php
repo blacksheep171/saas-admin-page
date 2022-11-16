d2db46c3f16d8afec201a8e723b5d0891e5df9ee
Hoahc <hoahigh.1109@gmail.com>
2022-11-16 10:02:47 +0700

    update : modify code.

---
+++ sidebar.php
@@ -0,0 +1,197 @@
+<div class="left-side-bar">
+	<div class="brand-logo">
+		<a href="index.html">
+			<img src="vendors/images/download.svg" alt="" class="dark-logo" />
+			<img src="vendors/images/deskapp-logo-white.svg" alt="" class="light-logo" />
+		</a>
+		<div class="close-sidebar" data-toggle="left-sidebar-close">
+			<i class="ion-close-round"></i>
+		</div>
+	</div>
+	<div class="menu-block customscroll">
+		<div class="sidebar-menu">
+			<ul id="accordion-menu">
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-house"></span><span class="mtext">Home</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="index3.php">Dashboard</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-table"></span><span class="mtext">Users</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="">Manage Users</a></li>
+						<li><a class="dropdown-toggle" href="javascript:;">Business</a>
+							<ul class="submenu">
+								<li><a href="">Manage Business</a></li>
+								<li><a href="">Manage Business Service</a></li>
+							</ul>
+						</li>
+						<li><a href="">Manage Company</a></li>
+						<li><a href="">Manage Subscriber</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-table"></span><span class="mtext">Listings</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="">Manage Listings</a></li>
+						<li><a href="">Listing Seller</a></li>
+						<li><a href="">Market Boss >> Manage Interested Listings</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-table"></span><span class="mtext">Templates</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="">Message templates</a></li>
+						<li><a href="">Notification templates</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-table"></span><span class="mtext">System</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="">Settings</a></li>
+						<li><a href="">Cache</a></li>
+						<li><a href="">Session</a></li>
+						<li><a href="">Phpinfo</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-table"></span><span class="mtext">Utils</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="">Base 64</a></li>
+						<li><a href="">UI Manager - Generate URL</a></li>
+						<li><a href="">UI Manager - Calendar</a></li>
+						<li><a href="">UI Manager - Master Template</a></li>
+						<li><a href="">Features - Manage</a></li>
+						<li><a href="">Features - Manage Invitation Code</a></li>
+						<li><a href="">Features - Generate Invitation Code</a></li>
+						<li><a href="">Features - Bulk Invitation</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-table"></span><span class="mtext">Signup</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="">Manage</a></li>
+						<li><a href="">Newsletter</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-table"></span><span class="mtext">Service</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="">Service Management</a></li>
+						<li><a href="">Quick Order Management</a></li>
+						<li><a href="">Order Management</a></li>
+						<li><a href="">Settings</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-table"></span><span class="mtext">RFP</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="">Request Lender</a></li>
+						<li><a href="">Request Marketing</a></li>
+						<li><a class="dropdown-toggle" href="javascript:;">Seller Journey</a>
+							<ul class="submenu">
+								<li><a href="">Seller - Steps Management </a></li>
+								<li><a href="">Seller - Content Management </a></li>
+								<li><a href="">Seller - Questions</a></li>
+								<li><a href="">Seller - Service Proposals</a></li>
+								<li><a href="">Seller - Explore Options</a></li>
+								<li><a href="">Seller - Explore Options Choice</a></li>
+								<li><a href="">Seller - Broker Request Management (Step 2)</a></li>
+								<li><a href="">Seller - Requests For Proposal</a></li>
+							</ul>
+						</li>
+						<li><a class="dropdown-toggle" href="javascript:;">Buyer Journey</a>
+							<ul class="submenu">
+								<li><a href="">Buyer - Self-prequals</a></li>
+								<li><a href="">Buyer - Self-prequals Result</a></li>
+								<li><a href="">Buyer - Schedule Lender Consultations</a></li>
+								<li><a href="">Buyer - Requests For Proposal</a></li>
+								<li><a href="">Buyer - FAQs Content</a></li>
+								<li><a href="">Buyer - Helper Videos</a></li>
+								<li><a href="">Buyer - Steps Management</a></li>
+								<li><a href="">Buyer - Content Management</a></li>
+								<li><a href="">Buyer - Question Management</a></li>
+								<li><a href="">Buyer - Request Pre Approval</a></li>
+								<li><a href="">Buyer - Closing Timeline</a></li>
+								<li><a href="">Buyer - Hire User</a></li>
+								<li><a href="">Buyer - Signature Management</a></li>
+								<li><a href="">Buyer - Package Option</a></li>
+								<li><a href="">Buyer - Package Option Order</a></li>
+							</ul>
+						</li>
+						<li><a href="">RFP Settings</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-table"></span><span class="mtext">Other</span>
+					</a>
+					<ul class="submenu">
+						<li> <a href="" aria-expanded="false">Buyer - Self-prequals Result</a></li>
+						<li> <a href="" aria-expanded="false">Contact</a></li>
+						<li> <a href="" aria-expanded="false">Onboarding Screens</a></li>
+						<li> <a href="" aria-expanded="false">Articles</a></li>
+						<li> <a href="" aria-expanded="false">Testimonial</a></li>
+						<li> <a href="" aria-expanded="false">Dynamic Link</a></li>
+						<li> <a href="" aria-expanded="false">Faqs</a></li>
+						<li> <a href="" aria-expanded="false">Challenges</a></li>
+						<li> <a href="" aria-expanded="false">Restaurants</a></li>
+						<li> <a href="" aria-expanded="false">API KEY Management</a></li>
+						<li> <a href="" aria-expanded="false">Media</a></li>
+					</ul>
+				</li>
+				<li>
+					<div class="dropdown-divider"></div>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-textarea-resize"></span><span class="mtext">Forms</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="form-basic.php">Form Basic</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-table"></span><span class="mtext">Tables</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="basic-table.php">Basic Tables</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="calendar.html" class="dropdown-toggle no-arrow">
+						<span class="micon bi bi-calendar4-week"></span><span class="mtext">Calendar</span>
+					</a>
+				</li>
+				<li class="dropdown">
+					<a href="javascript:;" class="dropdown-toggle">
+						<span class="micon bi bi-bug"></span><span class="mtext">Error Pages</span>
+					</a>
+					<ul class="submenu">
+						<li><a href="404.php">404</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
