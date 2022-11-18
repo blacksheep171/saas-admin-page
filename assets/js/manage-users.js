c486dadf031c39f608cb5f4846c3df23f9956220
Hoahc <hoahigh.1109@gmail.com>
2022-11-18 17:14:16 +0700

    update : clean code and create table add/edit user.

--- assets/js/manage-users.js
+++ assets/js/manage-users.js
@@ -1,175 +1,215 @@
-$((function() {
-    "use strict";
-    var t = $(".invoice-list-table")
-      , a = "../../../app-assets/"
-      , e = "../../pages/actions/table-user.php"
-      , n = "../../pages/actions/form-user.php"
-      , s = "../../pages/actions/form-user.php";
-    if ("laravel" === $("body").attr("data-framework") && (a = $("body").attr("data-asset-path"),
-    e = a + "app/invoice/preview",
-    n = a + "app/invoice/add",
-    s = a + "app/invoice/edit"),
-    t.length)
-        t.DataTable({
-            ajax:"https://reqres.in/api/users",
-            autoWidth: !1,
-            // columns: [{
-            //     data: "responsive_id"
-            // }, {
-            //     data: "invoice_id"
-            // }, {
-            //     data: "invoice_status"
-            // }, {
-            //     data: "issued_date"
-            // }, {
-            //     data: "client_name"
-            // }, {
-            //     data: "total"
-            // }, {
-            //     data: "balance"
-            // }, {
-            //     data: "invoice_status"
-            // }, {
-            //     data: ""
-            // }],
-            columns: [{
-                data: "id"
-            }, {
-                data: "avatar"
-            }, {
-                data: "first_name"
-            }, {
-                data: "last_name"
-            }, {
-                data: "email"
-            }],
-            columnDefs: [{
-                className: "control",
-                responsivePriority: 2,
-                targets: 0
-            }, {
-                targets: 1,
-                width: "46px",
-                render: function(t, a, n, s) {
-                    var i = n.id;
-                    return '<a class="fw-bold" href="' + e + '"> #' + i + "</a>"
+
+$(function () {
+    'use strict';
+
+    var dtUserTable = $('.user-list-table'),
+        assetPath = '../../../app-assets/',
+        userPreview = 'table-user.php',
+        userAdd = 'form-user.php',
+        userEdit = 'form-user.php';
+
+    if ($('body').attr('data-framework') === 'laravel') {
+        assetPath = $('body').attr('data-asset-path');
+        userPreview = assetPath + '../../pages/actions/';
+        userAdd = assetPath + '../../pages/actions/';
+        userEdit = assetPath + '../../pages/actions/';
+    }
+
+    if (dtUserTable.length) {
+        var dtUser = dtUserTable.DataTable({
+            ajax: "https://reqres.in/api/users",
+            autoWidth: false,
+            columns: [
+                { data: 'id' },
+                { data: 'avatar' },
+                { data: 'first_name' },
+                { data: 'last_name' },
+                { data: 'email' },
+                { data: '' }
+            ],
+            columnDefs: [
+                {
+                    className: "control",
+                    responsivePriority: 2,
+                    targets: 0
+                }, {
+                    targets: 1,
+                    width: "46px",
+                    render: function (data, type, full, meta) {
+                        var i = full['id'];
+                        return '<a class="fw-bold" href="' + userPreview + '"> #' + i + "</a>"
+                    }
+                },
+                {
+                    targets: 2,
+                    responsivePriority: 3,
+                    width: '270px',
+                    render: function (data, type, full, meta) {
+                        var $name = full['first_name'],
+                            $email = full['email'],
+                            $image = full['avatar'],
+                            stateNum = Math.floor(Math.random() * 6),
+                            states = ['success', 'danger', 'warning', 'info', 'primary', 'secondary'],
+                            $state = states[stateNum],
+                            $name = full['first_name'],
+                            $initials = $name.match(/\b\w/g) || [];
+                        $initials = (($initials.shift() || '') + ($initials.pop() || '')).toUpperCase();
+                        if ($image) {
+                            var $output =
+                                '<img  src="' + $image + '" alt="Avatar" width="32" height="32">';
+                        } else {
+                            $output = '<div class="avatar-content">' + $initials + '</div>';
+                        }
+                        var colorClass = $image === '' ? ' bg-light-' + $state + ' ' : ' ';
+
+                        var $rowOutput =
+                            '<div class="d-flex justify-content-left align-items-center">' +
+                            '<div class="avatar-wrapper">' +
+                            '<div class="avatar' +
+                            colorClass +
+                            'me-50">' +
+                            $output +
+                            '</div>' +
+                            '</div>' +
+                            '<div class="d-flex flex-column">' +
+                            '<h6 class="user-name text-truncate mb-0">' +
+                            $name +
+                            '</h6>' +
+                            '<small class="text-truncate text-muted">' +
+                            $email +
+                            '</small>' +
+                            '</div>' +
+                            '</div>';
+                        return $rowOutput;
+                    }
+                },
+                {
+                    targets: 3,
+                    width: '73px',
+                    render: function (data, type, full, meta) {
+                        var $firstName = full['first_name'];
+                        return '<span class="d-none">' + $firstName + '</span>' + $firstName;
+                    }
+                },
+                {
+                    targets: 4,
+                    width: '73px',
+                    render: function (data, type, full, meta) {
+                        var $lastName = full['last_name'];
+                        return '<span class="d-none">' + $lastName + '</span>' + $lastName;
+                    }
+                },
+                {
+                    targets: 5,
+                    width: '73px',
+                    render: function (data, type, full, meta) {
+                        var $email = full['email'];
+                        return '<span class="d-none">' + $email + '</span>' + $email;
+                    }
+                },
+                {
+                    targets: 6,
+                    title: 'Actions',
+                    width: '80px',
+                    orderable: false,
+                    render: function (data, type, full, meta) {
+                        var $id = full['id'];
+                        return '<div class="d-flex align-items-center col-actions"></a><a href="' + userPreview + '" data-bs-toggle="tooltip" data-bs-placement="top">' + feather.icons.eye.toSvg({
+                            class: "font-small-4 me-50"
+                        }) + '</a><a href="' + userEdit + '?id=' + $id + '" data-bs-toggle="tooltip" data-bs-placement="top">' + feather.icons.edit.toSvg({
+                            class: "font-small-4 me-50"
+                        }) + '</a><a href="#" data-bs-toggle="tooltip" data-bs-placement="top">' + feather.icons.trash.toSvg({
+                            class: "font-small-4 me-50"
+                        }) + '</a>' + "</div>"
+                    }
                 }
-            }, {
-                targets: 2,
-                responsivePriority: 3,
-                width: "270px",
-                render: function(t, e, n, s) {
-                    var i = n.first_name
-                      , o = n.email
-                      , l = n.avatar
-                      , d = ["success", "danger", "warning", "info", "primary", "secondary"][Math.floor(6 * Math.random())]
-                      , r = (i = n.first_name).match(/\b\w/g) || [];
-                    if (r = ((r.shift() || "") + (r.pop() || "")).toUpperCase(),
-                    l)
-                        var c = '<img  src="' + l + '" alt="Avatar" width="32" height="32">';
-                    else
-                        c = '<div class="avatar-content">' + r + "</div>";
-                    return '<div class="d-flex justify-content-left align-items-center"><div class="avatar-wrapper"><div class="avatar' + ("" === l ? " bg-light-" + d + " " : " ") + 'me-50">' + c + '</div></div><div class="d-flex flex-column"><h6 class="user-name text-truncate mb-0">' + i + '</h6><small class="text-truncate text-muted">' + o + "</small></div></div>"
-                }
-            }, {
-                targets: 3,
-                width: "73px",
-                render: function(t, a, e, n) {
-                    var s = e.first_name;
-                    return '<span class="d-none">' + s + "</span>" + s
-                }
-            }, 
-            {
-                targets: 4,
-                width: "73px",
-                render: function(t, a, e, n) {
-                    var s = e.last_name;
-                    return '<span class="d-none">' + s + "</span>" + s
-                }
-            },{
-                targets: 5,
-                width: "73px",
-                render: function(t, a, e, n) {
-                    var s = e.email;
-                    return '<span class="d-none">' + s + "</span>" + s
-                }
-            }, {
-                targets: -1,
-                title: "Actions",
-                width: "80px",
-                orderable: !1,
-                render: function(t, a, n, i) {
-                    return '<div class="d-flex align-items-center col-actions"><a class="me-1" href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Send Mail">' + feather.icons.send.toSvg({
-                        class: "font-medium-2 text-body"
-                    }) + '</a><a class="me-25" href="' + e + '" data-bs-toggle="tooltip" data-bs-placement="top" title="Preview Invoice">' + feather.icons.eye.toSvg({
-                        class: "font-medium-2 text-body"
-                    }) + '</a><div class="dropdown"><a class="btn btn-sm btn-icon dropdown-toggle hide-arrow" data-bs-toggle="dropdown">' + feather.icons["more-vertical"].toSvg({
-                        class: "font-medium-2 text-body"
-                    }) + '</a><div class="dropdown-menu dropdown-menu-end"><a href="#" class="dropdown-item">' + feather.icons.download.toSvg({
-                        class: "font-small-4 me-50"
-                    }) + 'Download</a><a href="' + s + '" class="dropdown-item">' + feather.icons.edit.toSvg({
-                        class: "font-small-4 me-50"
-                    }) + 'Edit</a><a href="#" class="dropdown-item">' + feather.icons.trash.toSvg({
-                        class: "font-small-4 me-50"
-                    }) + 'Delete</a><a href="#" class="dropdown-item">' + feather.icons.copy.toSvg({
-                        class: "font-small-4 me-50"
-                    }) + "Duplicate</a></div></div></div>"
-                }
-            }],
-            order: [[1, "desc"]],
-            dom: '<"row d-flex justify-content-between align-items-center m-1"<"col-lg-6 d-flex align-items-center"l<"dt-action-buttons text-xl-end text-lg-start text-lg-end text-start "B>><"col-lg-6 d-flex align-items-center justify-content-lg-end flex-lg-nowrap flex-wrap pe-lg-1 p-0"f<"invoice_status ms-sm-2">>>t<"d-flex justify-content-between mx-2 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+            ],
+            order: [[1, 'desc']],
+            dom:
+                '<"row d-flex justify-content-between align-items-center m-1"' +
+                '<"col-lg-6 d-flex align-items-center"l<"dt-action-buttons text-xl-end text-lg-start text-lg-end text-start "B>>' +
+                '<"col-lg-6 d-flex align-items-center justify-content-lg-end flex-lg-nowrap flex-wrap pe-lg-1 p-0"f<"user_status ms-sm-2">>' +
+                '>t' +
+                '<"d-flex justify-content-between mx-2 row"' +
+                '<"col-sm-12 col-md-6"i>' +
+                '<"col-sm-12 col-md-6"p>' +
+                '>',
             language: {
-                sLengthMenu: "Show _MENU_",
-                search: "Search",
-                searchPlaceholder: "Search Invoice",
+                sLengthMenu: 'Show _MENU_',
+                search: 'Search',
+                searchPlaceholder: 'Search User',
                 paginate: {
-                    previous: "&nbsp;",
-                    next: "&nbsp;"
+                    previous: '&nbsp;',
+                    next: '&nbsp;'
                 }
             },
-            buttons: [{
-                text: "Add Record",
-                className: "btn btn-primary btn-add-record ms-2",
-                action: function(t, a, e, s) {
-                    window.location = n
+            buttons: [
+                {
+                    text: 'Add Record',
+                    className: 'btn btn-primary btn-add-record ms-2',
+                    action: function (e, dt, button, config) {
+                        window.location = userAdd;
+                    }
                 }
-            }],
+            ],
             responsive: {
                 details: {
                     display: $.fn.dataTable.Responsive.display.modal({
-                        header: function(t) {
-                            return "Details of " + t.data().email
+                        header: function (row) {
+                            var data = row.data();
+                            return 'Details of ' + data['client_name'];
                         }
                     }),
-                    type: "column",
-                    renderer: function(t, a, e) {
-                        var n = $.map(e, (function(t, a) {
-                            return 2 !== t.columnIndex ? '<tr data-dt-row="' + t.rowIdx + '" data-dt-column="' + t.columnIndex + '"><td>' + t.title + ":</td> <td>" + t.data + "</td></tr>" : ""
-                        }
-                        )).join("");
-                        return !!n && $('<table class="table"/>').append("<tbody>" + n + "</tbody>")
+                    type: 'column',
+                    renderer: function (api, rowIdx, columns) {
+                        var data = $.map(columns, function (col, i) {
+                            return col.columnIndex !== 2
+                                ? '<tr data-dt-row="' +
+                                col.rowIdx +
+                                '" data-dt-column="' +
+                                col.columnIndex +
+                                '">' +
+                                '<td>' +
+                                col.title +
+                                ':' +
+                                '</td> ' +
+                                '<td>' +
+                                col.data +
+                                '</td>' +
+                                '</tr>'
+                                : '';
+                        }).join('');
+                        return data ? $('<table class="table"/>').append('<tbody>' + data + '</tbody>') : false;
                     }
                 }
             },
-            initComplete: function() {
-                $(document).find('[data-bs-toggle="tooltip"]').tooltip(),
-                this.api().columns(6).every((function() {
-                    var t = this
-                      , a = $('<select id="UserRole" class="form-select ms-50 text-capitalize"><option value=""> Select Status </option></select>').appendTo(".invoice_status").on("change", (function() {
-                        var a = $.fn.dataTable.util.escapeRegex($(this).val());
-                        t.search(a ? "^" + a + "$" : "", !0, !1).draw()
-                    }
-                    ));
-                    t.data().unique().sort().each((function(t, e) {
-                        a.append('<option value="' + t + '" class="text-capitalize">' + t + "</option>")
-                    }
-                    ))
-                }
-                ))
+            initComplete: function () {
+                $(document).find('[data-bs-toggle="tooltip"]').tooltip();
+                this.api()
+                    .columns(7)
+                    .every(function () {
+                        var column = this;
+                        var select = $(
+                            '<select id="UserRole" class="form-select ms-50 text-capitalize"><option value=""> Select Status </option></select>'
+                        )
+                            .appendTo('.user_status')
+                            .on('change', function () {
+                                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                                column.search(val ? '^' + val + '$' : '', true, false).draw();
+                            });
+
+                        column
+                            .data()
+                            .unique()
+                            .sort()
+                            .each(function (d, j) {
+                                select.append('<option value="' + d + '" class="text-capitalize">' + d + '</option>');
+                            });
+                    });
             },
-            drawCallback: function() {
-                $(document).find('[data-bs-toggle="tooltip"]').tooltip()
+            drawCallback: function () {
+                $(document).find('[data-bs-toggle="tooltip"]').tooltip();
             }
-        })
-}
-));
+        });
+    }
+});
